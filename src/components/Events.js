c0a86cdbae15727cf4485975987b5455f8c1d475
togami <tuabobo123@gmail.com>
2020-06-01 11:53:31 +0900

    fix title

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,69 +1,71 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import Event from './Event'
-import AppContext from '../contexts/AppContext'
+import Event from "./Event";
+import AppContext from "../contexts/AppContext";
 
-import { CircularProgressbar, buildStyles, CircularProgressbarWithChildren } from 'react-circular-progressbar';
-import '../style.css';
-import { easeQuadInOut } from 'd3-ease';
+import {
+  CircularProgressbar,
+  buildStyles,
+  CircularProgressbarWithChildren,
+} from "react-circular-progressbar";
+import "../style.css";
+import { easeQuadInOut } from "d3-ease";
 import AnimatedProgressProvider from "./AnimatedProgressProvider";
-import shortid from 'shortid';
+import shortid from "shortid";
 
 const Events = () => {
+  const { state } = useContext(AppContext);
 
-    const { state } = useContext(AppContext)
-
-    const progress = state.events.length > 0 ? ((state.events[0].count / state.events.length) * 100).toFixed(1) : 0;
-    return (
-        <div className="container-lg">
-            <div className="row">
-                <div className="col">
-                    <h4>イベント一覧</h4>
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>ID</th>
-                                <th>タイトル</th>
-                                <th>ボディー</th>
-                            </tr>
-                        </thead>
-                        <tbody >
-                            {state.events.map((event, index) => {
-                                return (
-                                    <Event event={event} key={index} />
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="col">
-                    <AnimatedProgressProvider
-                        valueStart={0}
-                        valueEnd={`${progress}`}
-                        duration={2.0}
-                        easingFunction={easeQuadInOut}
-
-                    >
-                        {(value) => {
-                            const roundedValue = Math.round(value);
-                            return (
-                                <>
-                                    <h1>Progress</h1>
-                                    <CircularProgressbar
-                                        value={value}
-                                        text={`${progress}%`}
-                                        styles={buildStyles({ pathTransition: 'none' })}
-                                    />
-                                </>
-                            );
-                        }}
-                    </AnimatedProgressProvider>
-                </div>
-            </div>
+  const progress =
+    state.events.length > 0
+      ? ((state.events[0].count / state.events.length) * 100).toFixed(1)
+      : 0;
+  return (
+    <div className="container-lg">
+      <div className="row">
+        <div className="col">
+          <h4>イベント一覧</h4>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th></th>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.events.map((event, index) => {
+                return <Event event={event} key={index} />;
+              })}
+            </tbody>
+          </table>
         </div>
+        <div className="col">
+          <AnimatedProgressProvider
+            valueStart={0}
+            valueEnd={`${progress}`}
+            duration={2.0}
+            easingFunction={easeQuadInOut}
+          >
+            {(value) => {
+              const roundedValue = Math.round(value);
+              return (
+                <>
+                  <h1>Progress</h1>
+                  <CircularProgressbar
+                    value={value}
+                    text={`${progress}%`}
+                    styles={buildStyles({ pathTransition: "none" })}
+                  />
+                </>
+              );
+            }}
+          </AnimatedProgressProvider>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Events
+export default Events;
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,44 +1,47 @@
 import {
-    CREATE_EVENT,
-    DELETE_ALL_EVENTS,
-    DELETE_EVENT,
-    REVERSE_DONE_FLAG
-} from '../actions/index'
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  DELETE_EVENT,
+  REVERSE_DONE_FLAG,
+} from "../actions/index";
 
 import shortid from "shortid";
 
-
-
-
-
 const events = (state = [], action) => {
+  switch (action.type) {
+    case CREATE_EVENT:
+      const event = {
+        title: action.title,
+        body: action.body,
+        isChecked: action.isChecked,
+        count: action.count,
+      };
+      const id = shortid.generate();
+      return [...state, { id, ...event }];
 
-    switch (action.type) {
-        case CREATE_EVENT:
-            const event = { title: action.title, body: action.body, isChecked: action.isChecked, count: action.count };
-            const id = shortid.generate();
-            return [...state, { id, ...event }]
+    case DELETE_EVENT:
+      state.map((event) =>
+        event.id !== action.id ? event.count : (event.count -= 1)
+      );
+      return state.filter((event) => event.id !== action.id); //()内に残って欲しいものをかく
+    case DELETE_ALL_EVENTS:
+      return [];
 
-        case DELETE_EVENT:
-            state.map((event) => event.id !== action.id ? event.count : event.count -= 1)
-            return state.filter(event => event.id !== action.id)//()内に残って欲しいものをかく
-        case DELETE_ALL_EVENTS:
-            return []
+    case REVERSE_DONE_FLAG:
+      state = state.map((event) => {
+        return {
+          ...event,
+          isChecked:
+            event.id === action.id ? !event.isChecked : event.isChecked,
+        };
+      });
+      const checked = state.filter((event) => event.isChecked);
+      state.map((event) => (event.count = checked.length));
 
-        case REVERSE_DONE_FLAG:
-            state = state.map((event) => {
-                return {
-                    ...event,
-                    isChecked: event.id === action.id ? !event.isChecked : event.isChecked
-                }
-            })
-            const checked = state.filter((event) => event.isChecked);
-            state.map((event) => event.count = checked.length);
+      return state;
+    default:
+      return state;
+  }
+};
 
-            return state
-        default:
-            return state
-    }
-}
-
-export default events
+export default events;
